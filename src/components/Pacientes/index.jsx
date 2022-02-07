ece54397585cb319b77c0e45a64dc7b73163e2aa
Andres Jaramillo <JaramilloG1999@outlook.com>
2022-02-06 20:47:46 -0500

    feat: creating structure and style of the patient list

--- src/components/Pacientes/index.jsx
+++ src/components/Pacientes/index.jsx
@@ -1,12 +1,23 @@
 import "./styles.scss";
-const Pacientes = () => {
+import Paciente from "./paciente";
+
+const ListadoPacientes = () => {
   return (
     <>
-      <div>
-        <h2>Pacientes</h2>
+      <div className="md:w-1/2 lg:w-3/5">
+        <h2 className="font-black text-3xl text-center mb-1">Listado Pacientes</h2>
+        <p className="text-xl text-center mb-5 ">
+          Administra tus {""}
+          <span className="text-indigo-600 font-bold"> Pacientes</span>
+        </p>
+        <div className="grid md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-2">
+          <Paciente />
+          <Paciente />
+          <Paciente />
+        </div>
       </div>
     </>
   );
 };
 
-export default Pacientes;
+export default ListadoPacientes;
