8e0da9854bb3306fe3084ab5c8c7c87eb9aa238f
Andres Jaramillo <JaramilloG1999@outlook.com>
2022-03-03 02:04:40 -0500

    feat: create component form patient

--- src/components/Formulario/index.jsx
+++ src/components/Formulario/index.jsx
@@ -1,17 +1,73 @@
-import "./styles.scss";
-const Formulario = () => {
+import { useState } from "react";
+import Swal from "sweetalert2";
+import { v4 as uuidv4 } from "uuid";
+const Formulario = ({ patients, setPatients }) => {
+  const uniqueId = uuidv4();
+  const [patient, setPatient] = useState({
+    id: uniqueId,
+    pet: "",
+    owner: "",
+    phone: "",
+    email: "",
+    date: "",
+    symptom: "",
+  });
+
+  const resetForm = () => {
+    setPatient({
+      pet: "",
+      owner: "",
+      phone: "",
+      email: "",
+      date: "",
+      symptom: "",
+    }); // Reset form
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    /* Validate form */
+
+    if (
+      patient.pet === "" ||
+      patient.owner === "" ||
+      patient.phone === "" ||
+      patient.email === "" ||
+      patient.date === "" ||
+      patient.symptom === ""
+    ) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        confirmButtonText: "Ok",
+        text: "All fields are required!",
+      });
+    } else {
+      resetForm();
+      Swal.fire({
+        icon: "success",
+        timer: 1500,
+        text: "Patient added successfully!",
+      });
+      setPatients([...patients, patient]);
+    }
+  };
+
   return (
     <>
-      <div className="md:w-1/2 lg:w-3/5 xl:w-2/5 mx-3">
+      <div className="md:w-1/2 lg:w-3/5 xl:w-2/5 mx-5 mb-5">
         <h1 className="font-black text-center text-3xl mb-1">
           Seguimiento Pacientes
         </h1>
         <p className="text-lg text-center mb-5">
           Añade Pacientes y {""}
-          <span className="text-indigo-600 font-bold">Administralos</span>
+          <span className="text-indigo-600 font-bold">Administrarlos</span>
         </p>
 
-        <form className="bg-white shadow-md rounded-xl py-10 px-5">
+        <form
+          className="bg-white shadow-md rounded-xl py-10 px-5"
+          onSubmit={handleSubmit}
+        >
           <div className="mb-5">
             <label
               htmlFor="mascota"
@@ -24,6 +80,8 @@ const Formulario = () => {
               type="text"
               className="border-2 w-full p-2 mt-2 placeholder:placeholder-gray-400 rounded-md"
               placeholder="Nombre Mascota"
+              value={patient.pet}
+              onChange={(e) => setPatient({ ...patient, pet: e.target.value })}
             />
           </div>
           <div className="mb-5">
@@ -38,6 +96,28 @@ const Formulario = () => {
               type="text"
               className="border-2 w-full p-2 mt-2 placeholder:placeholder-gray-400 rounded-md"
               placeholder="Nombre del Propietario"
+              value={patient.owner}
+              onChange={(e) =>
+                setPatient({ ...patient, owner: e.target.value })
+              }
+            />
+          </div>
+          <div className="mb-5">
+            <label
+              htmlFor="propietario"
+              className="block text-gray-700 uppercase font-bold"
+            >
+              Numero de Teléfono
+            </label>
+            <input
+              id="propietario"
+              type="text"
+              className="border-2 w-full p-2 mt-2 placeholder:placeholder-gray-400 rounded-md"
+              placeholder="Numero de Teléfono"
+              value={patient.phone}
+              onChange={(e) =>
+                setPatient({ ...patient, phone: e.target.value })
+              }
             />
           </div>
           <div className="mb-5">
@@ -52,6 +132,10 @@ const Formulario = () => {
               type="email"
               className="border-2 w-full p-2 mt-2 placeholder:placeholder-gray-400 rounded-md"
               placeholder="Email Contacto Propietario"
+              value={patient.email}
+              onChange={(e) =>
+                setPatient({ ...patient, email: e.target.value })
+              }
             />
           </div>
           <div className="mb-5">
@@ -65,19 +149,25 @@ const Formulario = () => {
               id="alta"
               type="date"
               className="border-2 w-full p-2 mt-2 placeholder:placeholder-gray-400 rounded-md"
+              value={patient.date}
+              onChange={(e) => setPatient({ ...patient, date: e.target.value })}
             />
           </div>
           <div className="mb-5">
             <label
-              htmlFor="sintomas"
+              htmlFor="síntomas"
               className="block text-gray-700 uppercase font-bold"
             >
-              Sintomas
+              Síntomas
             </label>
             <textarea
-              id="sintomas"
-              placeholder="Describe los sintomas"
+              id="síntomas"
+              placeholder="Describe los síntomas"
               className="border-2 w-full p-2 mt-2 placeholder:placeholder-gray-400 rounded-md"
+              value={patient.symptom}
+              onChange={(e) =>
+                setPatient({ ...patient, symptom: e.target.value })
+              }
             />
           </div>
           <input
