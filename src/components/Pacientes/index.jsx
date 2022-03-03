204375da576dae9ee75824636bf162eebcf6aac3
Andres Jaramillo <JaramilloG1999@outlook.com>
2022-03-03 02:06:19 -0500

    feat: consume and validations patient

--- src/components/Pacientes/index.jsx
+++ src/components/Pacientes/index.jsx
@@ -1,21 +1,37 @@
-import "./styles.scss";
 import Paciente from "./paciente";
 
-const ListadoPacientes = () => {
+const ListadoPacientes = ({ patients }) => {
   return (
     <>
-      <div className="md:w-1/2 lg:w-3/5">
-        <h2 className="font-black text-3xl text-center mb-1">Listado Pacientes</h2>
-        <p className="text-xl text-center mb-5 ">
-          Administra tus {""}
-          <span className="text-indigo-600 font-bold"> Pacientes</span>
-        </p>
-        <div className="grid md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-2">
-          <Paciente />
-          <Paciente />
-          <Paciente />
+      {patients && patients.length === 0 ? (
+        <div className="md:w-1/2 lg:w-3/5">
+          <h2 className="font-black text-3xl text-center mb-1">
+            No hay pacientes
+          </h2>
+          <p className="text-xl text-center mb-5 ">
+            Añade pacientes {""}
+            <span className="text-indigo-600 font-bold">
+              {" "}
+              y visualiza su seguimiento
+            </span>
+          </p>
         </div>
-      </div>
+      ) : (
+        <div className="md:w-1/2 lg:w-3/5">
+          <h2 className="font-black text-3xl text-center mb-1">
+            Listado Pacientes
+          </h2>
+          <p className="text-xl text-center mb-5 ">
+            Administra tus {""}
+            <span className="text-indigo-600 font-bold"> Pacientes</span>
+          </p>
+          <div className="grid md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-2">
+            {patients.map((patient) => (
+              <Paciente key={patient.id} patient={patient} />
+            ))}
+          </div>
+        </div>
+      )}
     </>
   );
 };
--- src/components/Pacientes/paciente.jsx
+++ src/components/Pacientes/paciente.jsx
@@ -1,31 +1,45 @@
-const paciente = () => {
+const paciente = ({ patient }) => {
   return (
     <>
-      <div className="mb-3 mx-2 bg-white shadow-md px-5 py-5 rounded-xl">
+      <div className="mb-3 mx-5 bg-white shadow-md px-5 py-5 rounded-xl">
         <p className="font-bold mb-3 text-gray-700 uppercase">
-          Nombre: {""} <span className="normal-case font-normal">Hook</span>
+          Nombre:
+          <span className="normal-case font-normal"> {patient?.pet}</span>
         </p>
         <p className="font-bold mb-3 text-gray-700 uppercase">
-          Propietario: {""}
-          <span className="normal-case font-normal">Andres</span>
+          Propietario:
+          <span className="normal-case font-normal"> {patient?.owner}</span>
         </p>
         <p className="font-bold mb-3 text-gray-700 uppercase">
-          Email: {""}
-          <span className="normal-case font-normal">correo@correo.com</span>
+          Numero:
+          <span className="normal-case font-normal"> {patient?.phone}</span>
         </p>
         <p className="font-bold mb-3 text-gray-700 uppercase">
-          Fecha Alta: {""}
-          <span className="normal-case font-normal">6 Febrero de 2022</span>
+          Email:
+          <span className="normal-case font-normal"> {patient?.email}</span>
         </p>
         <p className="font-bold mb-3 text-gray-700 uppercase">
-          Síntomas: {""}
-          <span className="normal-case font-normal">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea modi eum
-            saepe. Possimus officia exercitationem voluptatem saepe quod ipsam
-            recusandae ullam voluptatum tempore amet modi temporibus, quae
-            facere optio? Dicta!
-          </span>
+          Fecha De Alta:
+          <span className="normal-case font-normal"> {patient?.date}</span>
         </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">
+          Síntomas:
+          <span className="normal-case font-normal"> {patient?.symptom}</span>
+        </p>
+        <div className="flex items-center justify-between mt-5">
+          <button
+            type="button"
+            className="py-2 px-5 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg transition-all"
+          >
+            editar
+          </button>
+          <button
+            type="button"
+            className="py-2 px-5 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg transition-all"
+          >
+            eliminar
+          </button>
+        </div>
       </div>
     </>
   );
